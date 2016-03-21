4a9be0c4ef581f869dea39fea4280fe59f175dca
gjohnson85 <greg.johnson.acct1@gmail.com>
2016-03-21 13:25:46 -0400

    Initial Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('app.controllers', [])
+  
+.controller('page2Ctrl', function($scope) {
+
+})
+      
+.controller('aboutSaintConnectionCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,35 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('page2', {
+    url: '/main',
+    templateUrl: 'templates/page2.html',
+    controller: 'page2Ctrl'
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('aboutSaintConnection', {
+    url: '/about',
+    templateUrl: 'templates/aboutSaintConnection.html',
+    controller: 'aboutSaintConnectionCtrl'
+  })
+
+$urlRouterProvider.otherwise('/main')
+
+  
+
+});
